3bb598462bee3fc5989b4f0eb166c0d4e55f3511
Himanshu <himanshusingh2087@gmail.com>
2026-04-15 21:24:53 +0530

    increase payload limit from 16kb to 10mb

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,8 +15,8 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: "Attendance API is successfully running on Vercel! 🚀" });
 });
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.static("public"))
 app.use(cookieParser())
 
